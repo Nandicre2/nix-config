374d3471cf597cb3274df94d42318a98d98a70aa
Nandicre <nandicre@protonmail.com>
2023-02-25 11:16:22 +0100

    hosts/common : refactor and add basic stuff

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -2,14 +2,13 @@
 { lib, inputs, outputs, ... }:
 {
   imports = [
+    inputs.impermanence.nixosModules.impermanence
     inputs.home-manager.nixosModules.home-manager
     ./nix.nix
   ];
 
   home-manager = {
-    users = {
-      nandicre = import ../../home-manager/home.nix;
-    };
+    users.nandicre = import ../../home-manager/home.nix;
     useUserPackages = true;
     extraSpecialArgs = { inherit inputs outputs; };
   };
@@ -21,5 +20,6 @@
     };
   };
 
+  programs.fuse.userAllowOther = true;
   hardware.enableRedistributableFirmware = true;
 }
