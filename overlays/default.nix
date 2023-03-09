f0a0c063cf71e143b5efb89912c05e75e8fff0b3
Nandicre <nandicre@protonmail.com>
2023-03-09 12:05:08 +0100

    podman-compose : overlay for devel branch

--- overlays/default.nix
+++ overlays/default.nix
@@ -10,5 +10,14 @@
     # example = prev.example.overrideAttrs (oldAttrs: rec {
     # ...
     # });
+    podman-compose = prev.podman-compose.overrideAttrs(oldAttrs: {
+      version = "1.0.4-dev";
+      src = final.fetchFromGitHub {
+        repo = "podman-compose";
+        owner = "containers";
+        rev = "08ffcf6126a3ae4016e3d81e963a3629e4b75986";
+        sha256 = "sha256-ybdwBc//clXQ8WHG3lfGP4g8VLECFvEWSnVxZxjhLLU=";
+      };
+    });
   };
 }
