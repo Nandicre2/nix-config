97c01206d8d792f7bd7ceec1520e66640df6631d
Nandicre <nandicre@protonmail.com>
2023-04-03 17:31:04 +0200

    flake : remove arm part

--- flake.nix
+++ flake.nix
@@ -22,7 +22,7 @@
   outputs = { self, nixpkgs, home-manager, ... }@inputs:
     let
       inherit (self) outputs;
-      forEachSystem = nixpkgs.lib.genAttrs [ "aarch64-linux" "x86_64-linux" ];
+      forEachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
       forEachPkgs = f: forEachSystem (sys: f nixpkgs.legacyPackages.${sys});
     in
     {
