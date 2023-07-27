4728fdb2d157b7732dbb8f249574dfc54b221164
Nandicre <nandicre@protonmail.com>
2023-07-28 00:12:13 +0200

    flake : comment and simplify

--- flake.nix
+++ flake.nix
@@ -1,53 +1,45 @@
 {
   description = "Nandicre NixOS configuration";
 
-  # To update all inputs:
-  # $ nix flake update
+  # To update all inputs: 'nix flake update'
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    # Home-manager (https://github.com/nix-community/home-manager)
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # NixOS-hardware (https://github.com/NixOS/nixos-hardware/)
     hardware.url = "github:nixos/nixos-hardware";
 
+    # Manage disks (https://github.com/nix-community/disko)
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:
-    let
-      inherit (self) outputs;
-      forEachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
-      forEachPkgs = f: forEachSystem (sys: f nixpkgs.legacyPackages.${sys});
-    in
-    {
-      # Devshell for bootstrapping
-      # Acessible through 'nix develop' or 'nix-shell' (legacy)
-      devShells = forEachPkgs (pkgs: import ./shell.nix { inherit pkgs; });
-
-      # NixOS configuration entrypoint
-      # Available through 'nixos-rebuild --flake .#your-hostname'
-      nixosConfigurations = {
-        # Primary Desktop (SFFPC)
-        ephaistos = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
-          modules = [ ./hosts/ephaistos ];
-        };
-        # Laptop VivoBook
-        aphrodite = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
-          modules = [ ./hosts/aphrodite ];
-        };
-        # Server - Old desktop
-        apollon = nixpkgs.lib.nixosSystem {
-          specialArgs = { inherit inputs outputs; };
-          modules = [ ./hosts/apollon ];
-        };
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    # Choose a config and build with 'nixos-rebuild --flake .#your-hostname'
+    # Home-manager is built with the system
+    nixosConfigurations = {
+      # Primary Desktop (SFFPC)
+      ephaistos = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ ./hosts/ephaistos ];
+      };
+      # Laptop VivoBook
+      aphrodite = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ ./hosts/aphrodite ];
+      };
+      #TODO Server - Old desktop
+      apollon = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ ./hosts/apollon ];
       };
     };
+  };
 }
