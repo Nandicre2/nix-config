c813bc36e11b1abec0f22c2c640e4749ea0258f7
Nandicre2 <nandicre@protonmail.com>
2023-08-03 10:56:49 +0200

    hosts/common : enable bluetooth

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -86,6 +86,8 @@ in
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
+  hardware.bluetooth.enable = true;
+
   # Enable sound with pipewire.
   sound.enable = true;
   hardware.pulseaudio.enable = false;
