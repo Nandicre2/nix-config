81406360ca430ade84bce0a92395e223e38ecfac
Nandicre2 <nandicre@protonmail.com>
2024-03-10 23:08:44 +0100

    change kde.nix

--- hosts/common/kde.nix
+++ hosts/common/kde.nix
@@ -14,8 +14,7 @@
     # Desktop Manager Configuration
     #---------------------------------------------------------------------
     desktopManager = {
-      plasma5.enable = true;
-
+      plasma6.enable = true;
     };
 
     #---------------------------------------------------------------------
@@ -27,30 +26,6 @@
         autoNumlock = true;
         wayland.enable = true;
       };
-      defaultSession = "plasmawayland";
     };
-
-    # ---------------------------------------------------------------------
-    # Video settings that go hand-in-hand with OpenGL
-    # ---------------------------------------------------------------------
-    videoDrivers = [
-      "fbdev" # The fbdev (Framebuffer Device) driver is a generic framebuffer driver that provides access to the frame buffer of the display hardware.
-      # "modesetting"     # The modesetting driver is a generic driver for modern video hardware that relies on kernel modesetting (KMS) to set the display modes and manage resolution and refresh rate.
-      # "amdgpu"          # This is the open-source kernel driver for AMD graphics cards. It's part of the AMDGPU driver stack and provides support for newer AMD GPUs.
-      # "nouveau"         # Nouveau is an open-source driver for NVIDIA graphics cards. It aims to provide support for NVIDIA GPUs and is an alternative to the proprietary NVIDIA driver
-      # "radeon"          # The Radeon driver is an open-source driver for AMD Radeon graphics cards. It provides support for older AMD GPUs.
-      "nvidia"
-    ];
   };
-
-  #---------------------------------------------------------------------
-  # System Packages Configuration
-  #---------------------------------------------------------------------
-  environment.systemPackages = with pkgs; [
-    libsForQt5.kaccounts-integration
-    libsForQt5.kaccounts-providers
-    libsForQt5.packagekit-qt
-    libportal-qt5
-  ];
-
 }
